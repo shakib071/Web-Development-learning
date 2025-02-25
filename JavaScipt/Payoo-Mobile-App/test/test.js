57920083ee42bb04a840c5bad21c760acda9bfe9
shakib071 <shakibhasan071@gmail.com>
2025-02-26 01:17:44 +0600

    payoo

---
+++ JavaScipt/Payoo-Mobile-App/test/test.js
@@ -0,0 +1,12 @@
+document.getElementById("bangladesh-btn").addEventListener("click", function () {
+    document.getElementById("bd").style.display = "block";
+
+    document.getElementById("usa").style.display = "none";
+    
+})
+
+document.getElementById("usa-btn").addEventListener("click", function () {
+    document.getElementById("bd").style.display = "none";
+
+    document.getElementById("usa").style.display = "block";
+})
